580eac735e9a88b1e9add0038acb4531ed2df371
s1-howie <howieh@sentinelone.com>
2022-06-25 10:29:35 -0400

    updated package selection criteria

--- s1-agent-helper.ps1
+++ s1-agent-helper.ps1
@@ -1,4 +1,5 @@
 #Requires -RunAsAdministrator
+# NOTE:  The auto_reboot parameter only applies to the EXE agents versions < 22.1
 param(
     [Parameter(Position=0,mandatory=$true)]
     [string]$s1_console_prefix,
@@ -12,18 +13,18 @@ param(
     [string]$auto_reboot
     )
 
-write-output "Console:          $s1_console_prefix"
-# write-output "API Key:          $api_key"
-# write-output "Site Token:       $site_token"
-write-output "Version Status:   $version_status"
-
-
+# Show how the input parameters will be used
+write-output ""
+write-output "Console:             $s1_console_prefix"
+write-output "Version Status:      $version_status"
 $s1_mgmt_url = "https://$s1_console_prefix.sentinelone.net"
-Write-Output "mgmt url:         $s1_mgmt_url"
+Write-Output "mgmt url:            $s1_mgmt_url"
 $api_endpoint = "/web/api/v2.1/update/agent/packages"
 $agent_file_name = ""
 $agent_download_link = ""
+$agent_package_major_version = ""
 
+# Basic sanity checks for input parameters
 if (-Not ($api_key.Length -eq 80)) {
     Write-Output "API Keys are generally 80 characters long and are alphanumeric."
     exit 1
@@ -40,19 +41,19 @@ if ($version_status -ne "GA" -and $version_status -ne "EA") {
     exit 1
 }
 
-# Concatenate URL with API Endpoint
+# Concatenate the Management Console URL with API Endpoint for Agent Packages
 $uri = $s1_mgmt_url + $api_endpoint
 
-# Convert Agent version status to lowercase
+# Convert Agent version status to lowercase (for usage in the upcoming API query)
 $version_status = $version_status.ToLower()
 
 # Check if we need a 32 or 64bit package
 $osArch = "64 bit"
 if($env:PROCESSOR_ARCHITECTURE -eq "x86"){$osArch = "32 bit"}
 
-
 # Configure HTTP header for API Calls
 $apiHeaders = @{"Authorization"="APIToken $api_key"}
+
 # The body contains parameters to search for packages with .exe file extensions.. ordering by latest major version.
 $body = @{
     "limit"=10
@@ -64,41 +65,46 @@ $body = @{
     "osArches"=$osArch
     "status"=$version_status
     }
+
 # Query the S1 API
 $response = Invoke-RestMethod -Uri $uri -Headers $apiHeaders -Method Get -ContentType "application/json" -Body $body
+
 # Store the response data as a list of objects
 $packages = $response.data
 
+# Find the package that matches our criteria and record the file name and download link.
 #Note: "$version_status*"" will match either GA or GA-SP1, GA-SP2, etc
 foreach ($package in $packages) {
     if ($package.status -like "$version_status*") {
         $agent_download_link = $package.link
         $agent_file_name = $package.fileName
+        $agent_package_major_version = $package.majorVersion
         break
     }
 }
 
+# Show which file name was selected and its download link.
 Write-Output "Agent File Name:     $agent_file_name"
 Write-Output "Agent Download Link: $agent_download_link"
+write-output ""
 
-# Now that we have the download link and file name.  Download the package.
+# Now that we have the download link and file name.  Download the package to a TEMP directory.
 $wc = New-Object System.Net.WebClient
 $wc.Headers['Authorization'] = "APIToken $api_key"
 $wc.DownloadFile($agent_download_link, "$env:TEMP\$agent_file_name")
 
-##### LOGIC TO HANDLE OLDER VERSIONS??? #####
-
-##### LOGIC TO HANDLE FLAGS (SERVER_PROXY, SERVER_PROXY_CREDENTIALS, IOC_PROXY, VDI, WSC)
-
-# Execute the package
-# if($auto_reboot -eq "True") {
-#     # Execute the package with the quiet option and force restart
-#     & "$env:TEMP\$agent_file_name" /SITE_TOKEN=$site_token /quiet /reboot
-# }
-# else {
-#     # Execute the package with the quiet option and do NOT restart
-#     & "$env:TEMP\$agent_file_name" /SITE_TOKEN=$site_token /quiet /norestart
-# }
-
-# Execute the package (22.1+)
-& "$env:TEMP\$agent_file_name" -t $site_token
+# If the agent package is version 22.1+, use the new CLI installation syntax
+if ($agent_package_major_version -ge "22.1") {
+    & "$env:TEMP\$agent_file_name" -t $site_token
+}
+else {
+    #Execute the older EXE package
+    if($auto_reboot -eq "True") {
+        # Execute the package with the quiet option and force restart
+        & "$env:TEMP\$agent_file_name" /SITE_TOKEN=$site_token /quiet /reboot
+    }
+    else {
+        # Execute the package with the quiet option and do NOT restart
+        & "$env:TEMP\$agent_file_name" /SITE_TOKEN=$site_token /quiet /norestart
+    }
+}
